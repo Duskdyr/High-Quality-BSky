88b65493c8e547fdcda1fecb0b85ea3306359994
Duskdyr <154408515+Duskdyr@users.noreply.github.com>
2025-01-25 04:11:39 -0700

    Create HD_Twitter.user.js

---
+++ HD_Twitter.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         HD Twitter
+// @author       Duskdyr
+// @description  Improves Twitter (X) image quality by replacing image formats. When clicking an image, JPEG images are replaced with lossless PNG images.
+// @match        *://*.x.com/*
+// @grant        none
+// @namespace    dskdr
+// @license      MIT
+// @version      0.9
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function imgrplcr() {
+        document.querySelectorAll('img').forEach(img => {
+            let url = img.src;
+
+            if (url.includes('format=jpg') && url.includes('name=large')) {
+                url = url.replace('=jpg', '=png');
+//                url = url.replace('/feed_thumbnail', '/feed_fullsize');
+            }
+            if (img.src !== url) {
+                img.src = url;
+            }
+        });
+    }
+
+    imgrplcr();
+
+    const observer = new MutationObserver(imgrplcr);
+    observer.observe(document.body, { childList: true, subtree: true });
+})();
